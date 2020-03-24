2f198a45322705a9d1408111a2e95317db7df43d
Jennifer Morales <jennifer.morales1201@gmail.com>
2020-03-24 15:54:38 -0400

    Added componentDidMount to the quote component

--- src/components/quote.jsx
+++ src/components/quote.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
 
 class Quote extends Component {
+    state = {
+        chuckQuote: "Chuck Norris can spawn threads that complete before they are started."
+    }
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                chuckQuote: "Google won't search for Chuck Norris because it knows you don't find Chuck Norris, he finds you."
+            })
+        }, 3000);
+    }
   render() {
+    const { chuckQuote } = this.state
     return (
       <div>
         <p>Chuck Says...</p>
-        <p>{this.props.newQuote}</p>
+        <p>{ chuckQuote }</p>
       </div>
     );
   }
