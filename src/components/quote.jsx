c941e99d8b233fd9b8553f6a8f2de93ba45f9ac9
Jennifer Morales <jennifer.morales1201@gmail.com>
2020-03-25 13:59:19 -0400

    changed quote component to pull data from api

--- src/components/quote.jsx
+++ src/components/quote.jsx
@@ -4,12 +4,15 @@ class Quote extends Component {
     state = {
         chuckQuote: "Chuck Norris can spawn threads that complete before they are started."
     }
-    componentDidMount() {
-        setTimeout(() => {
-            this.setState({
-                chuckQuote: "Google won't search for Chuck Norris because it knows you don't find Chuck Norris, he finds you."
-            })
-        }, 3000);
+   async componentDidMount() {
+     try {
+       const response = await fetch(`https://api.chucknorris.io/jokes/random?category=dev`);
+       const data = await response.json();
+       this.setState({ chuckQuote: data.value });
+     } catch(error) {
+       return error;
+     }
+
     }
   render() {
     const { chuckQuote } = this.state
